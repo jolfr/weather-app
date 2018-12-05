53c54129342762e5c94f47ccb5745e4330698399
jolfr <thomas.jack.carroll@gmail.com>
2018-12-04 22:30:36 -0500

    wrote tests for getGraphic, coverage is full

--- src/components/jsx/CardOverview.jsx
+++ src/components/jsx/CardOverview.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import "../css/DayCard.css";
+import Img from 'reac'
+
+export const getGraphic = (weather) => {
+    switch (weather) {
+        case 'FULL CLOUD':
+            return 'cloud.png';
+        case 'PART CLOUD':
+            return 'cloud_and_sun.png';
+        case 'RAIN':
+            return 'rain.png';
+        case 'SNOW':
+            return 'snow.png';
+        case 'SUN':
+            return 'sun.png';
+        case 'THUNDER':
+            return 'thunder.png';
+        default:
+            return 'sun.png';
+    }
+};
 
 class CardOverview extends React.Component {
     render() {
         return (
             <div className='Card'>
-                <h1 className="CardOverview">{this.props.day}</h1>
+                <div className="CardOverview">
+                    <div>
+                        <image ></image>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/tests/CardOverview.test.js
+++ src/components/tests/CardOverview.test.js
@@ -1,9 +1,49 @@
 import { shallow } from '../../enzyme.js'
 import React from 'react';
 import CardOverview from '../jsx/CardOverview';
+import {getGraphic} from "../jsx/CardOverview";
+
 import renderer from 'react-test-renderer';
 
 describe('Card Overview Tests' , () => {
+
+    describe('Card Overview Tests' , () => {
+        it('should return FULL CLOUD', function () {
+            const weather = 'FULL CLOUD';
+            const ret = 'cloud.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return PART CLOUD', function () {
+            const weather = 'PART CLOUD';
+            const ret = 'cloud_and_sun.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return RAIN', function () {
+            const weather = 'RAIN';
+            const ret = 'rain.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return SNOW', function () {
+            const weather = 'SNOW';
+            const ret = 'snow.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return SUN', function () {
+            const weather = 'SUN';
+            const ret = 'sun.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return THUNDER', function () {
+            const weather = 'THUNDER';
+            const ret = 'thunder.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+        it('should return default', function () {
+            const weather = 'ELSE';
+            const ret = 'sun.png';
+            expect(getGraphic(weather)).toMatch(ret);
+        });
+    });
 
     it('should have CSS',  () => {
         const wrapper = shallow(<CardOverview/>);
